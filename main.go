3b63d2e2e3f02e929a38415cefc8e78ae22a470f
mdecosto <mdecosto@futurex.ph>
2024-06-25 16:41:27 +0800

    working in 1 folder

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+// main.go
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -12,31 +20,84 @@ type Todo struct {
 	IsCompleted bool   `json:"isCompleted"`
 }
 
-var todos = []Todo{
-	{Id: 1, Name: "Learn Go", IsCompleted: false},
-	{Id: 2, Name: "Learn Alpine", IsCompleted: false},
-	{Id: 3, Name: "Go to the gym", IsCompleted: true},
-}
-
 var templates map[string]*template.Template
+var conn *pgx.Conn
 
-// Load templates on program initialisation
-// https://www.digitalocean.com/community/tutorials/understanding-init-in-go
 func init() {
+	godotenv.Load() // Load .env file
+
+	// Build the connection string
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Note: Be cautious with direct string manipulation for connection strings in production code to avoid issues with special characters.
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var err error
+	conn, err = pgx.Connect(context.Background(), connectionString)
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+
 	if templates == nil {
 		templates = make(map[string]*template.Template)
 	}
-
 	templates["index.html"] = template.Must(template.ParseFiles("index.html"))
+	templates["todo.html"] = template.Must(template.ParseFiles("todo.html"))
+}
+
+// fetch
+func fetchTodos() ([]Todo, error) {
+	var todos []Todo
+	rows, err := conn.Query(context.Background(), "SELECT id, name, is_completed FROM todos")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var t Todo
+		if err := rows.Scan(&t.Id, &t.Name, &t.IsCompleted); err != nil {
+			return nil, err
+		}
+		todos = append(todos, t)
+	}
+	return todos, nil
 }
 
 // handlers
+func submitTodoHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PostFormValue("name")
+	completed := r.PostFormValue("completed") == "true"
+	var lastInsertId int
+	err := conn.QueryRow(context.Background(), "INSERT INTO todos (name, is_completed) VALUES ($1, $2) RETURNING id", name, completed).Scan(&lastInsertId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	todo := Todo{Id: lastInsertId, Name: name, IsCompleted: completed}
+	tmpl := templates["todo.html"]
+	tmpl.ExecuteTemplate(w, "todo.html", todo)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	todos, err := fetchTodos()
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonTodos, err := json.Marshal(todos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	tmpl := templates["index.html"]
-	tmpl.ExecuteTemplate(w, "index.html", nil)
+	tmpl.ExecuteTemplate(w, "index.html", map[string]template.JS{"Todos": template.JS(jsonTodos)})
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/submit-todo/", submitTodoHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
